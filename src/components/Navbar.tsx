b5ee690b7b4dd481bf8374fe44f7b1c36e913126
Hashim332 <ppyhr5@nottingham.ac.uk>
2025-07-16 10:29:23 +0100

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,13 @@ function Navbar() {
 
   return (
     <>
-      <nav className="w-full bg-white shadow flex items-center justify-between px-4 py-3 mb-4 md:px-6 md:py-4 md:mb-8">
-        <a className="text-xl font-bold focus:outline-none" href="/">
+      <nav className='w-full bg-white shadow flex items-center justify-between px-4 py-3 mb-4 md:px-6 md:py-4 md:mb-8'>
+        <a className='text-xl font-bold focus:outline-none' href='/'>
           EV Calculator
         </a>
         <button
           onClick={() => setIsContactModalOpen(true)}
-          className="text-gray-700 hover:text-violet-500 transition-colors focus:outline-none"
+          className='text-gray-700 hover:text-violet-500 hover:cursor-pointer transition-colors focus:outline-none'
         >
           Contact
         </button>
